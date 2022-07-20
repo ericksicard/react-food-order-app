5df2b9f68828026d514c51f6fc28569eb5bf1ce5
ericksicard <sicard.erick@gmail.com>
2022-07-19 21:17:11 -0400

    Using useEffect Hook for a bumping button when adding an item to the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
-
+    const [btnHighlighted, setBtnHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
 
-    const numberOfCartItems = cartCtx.items.reduce( (cur, item) => {
+    const numberOfCartItems = items.reduce( (cur, item) => {
         return cur + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setBtnHighlighted(true);
+
+        const timer = setTimeout( () => {
+            setBtnHighlighted(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
