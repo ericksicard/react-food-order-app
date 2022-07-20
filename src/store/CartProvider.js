5ce24a54a4c6119d7ccbc56063659661f5e4d544
ericksicard <sicard.erick@gmail.com>
2022-07-19 20:20:03 -0400

    More complex Reducer logic to update existing items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -14,8 +14,24 @@ const defaultCartState = {
 // the "action" argument is the updated data passed to "cartReducer" from "dispatchCartAction" function at "useReducer"
 const cartReducer = (state, action) => {
     if(action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        
+        // Updating an existing item in the cart
+        const existingCartItemIndex = state.items.findIndex( item => item.id === action.item.id );
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+        
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
